61f5bcaace28591c314e3b0ceec54b76c7899d79
changtail <1062158990@qq.com>
2024-07-31 13:59:57 +0800

    修改了动画bug   删除了跳跃动作

--- Script/Player.gd
+++ Script/Player.gd
@@ -6,10 +6,8 @@ extends CharacterBody3D
 
 
 # 速度、跑步速度、跳跃力的变量
-const Speed := 500.0
-const RunSpeed := Speed * 2.0
-const JUMP_VELOCITY := 5.6
-
+@export var Speed := 300.0
+@export var RunSpeed := Speed * 1.5
 # 获取系统重力
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -20,13 +18,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# 按键触发跳跃
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# 判断输入事件、移动方向、移动速度
 	var input_shift := Input.is_action_pressed("shift")
-	var input_dir := Input.get_vector("a", "d", "w", "s")
+	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
 	if direction:
 		if input_shift:
@@ -43,8 +37,10 @@ func _physics_process(delta: float) -> void:
 		state_machine.travel("Idle")
 		
 	# 判断角色移动方向
-	if velocity.length() > 0.1:
+	if abs(velocity.x) + abs(velocity.z) > 0.1:
 		var v = Vector2(velocity.z, velocity.x)
 		player.rotation.y = v.angle()
-
+	if Input.is_action_pressed("B"):
+		position = Vector3.ZERO
+	
 	move_and_slide()
