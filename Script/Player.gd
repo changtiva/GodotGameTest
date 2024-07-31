dcb0ff68951ca4fff57a0386faa37e2b078fed09
changtail <1062158990@qq.com>
2024-07-31 11:26:51 +0800

    添加了角色动画树

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody3D
 
-const RunSpeed = 1000.0
-const Speed = 500.0
-const JUMP_VELOCITY = 4.5
+@onready var player: Node3D = $Player
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+# 速度、跑步速度、跳跃力的变量
+const Speed := 500.0
+const RunSpeed := Speed * 2.0
+const JUMP_VELOCITY := 5.6
+
+# 获取系统重力
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
 
-	# Add the gravity.
+	# 判断下坠
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
+	# 按键触发跳跃
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# 判断输入事件、移动方向、移动速度
 	var input_shift := Input.is_action_pressed("shift")
 	var input_dir := Input.get_vector("a", "d", "w", "s")
 	var direction := (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
@@ -27,11 +32,19 @@ func _physics_process(delta: float) -> void:
 		if input_shift:
 			velocity.x = direction.x * delta * RunSpeed
 			velocity.z = direction.z * delta * RunSpeed
+			state_machine.travel("Fast Run")
 		else:
 			velocity.x = direction.x * delta * Speed
 			velocity.z = direction.z * delta * Speed
+			state_machine.travel("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, Speed)
 		velocity.z = move_toward(velocity.z, 0, Speed)
+		state_machine.travel("Idle")
+		
+	# 判断角色移动方向
+	if velocity.length() > 0.1:
+		var v = Vector2(velocity.z, velocity.x)
+		player.rotation.y = v.angle()
 
 	move_and_slide()
